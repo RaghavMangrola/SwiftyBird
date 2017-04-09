553eb828ad3a5c13b6ef0a820f6e208113c6cb48
Raghav Mangrola <raghav.mangrola@gmail.com>
2017-04-08 17:22:51 -0700

    Add SlackKit as a dependency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyBird"
+  name: "SwiftyBird",
+  dependencies: [
+    .Package(url: "https://github.com/pvzig/SlackKit.git", majorVersion: 3)
+  ]
 )
